2853fe4963226fd76dd3091b797bca66cb936b9d
unknown <william.amorim@FB011736.ibutantan.local>
2015-01-27 17:55:09 -0200

    scipt aula 5

---
+++ script/aula_05.R
@@ -0,0 +1,61 @@
+### Script --- Aula 5 --- ggplot2
+
+library(ggplot2)
+library(dplyr)
+library(magrittr)
+library(tidyr)
+
+
+### Banco de dados: Flow of the River Nile
+#################################################
+
+Nile
+
+class(Nile)
+
+# Fazer um gráfico da série
+
+
+
+
+### Banco de dados: Accidental Deaths in the US 1973-1978
+###########################################################
+
+USAccDeaths
+
+# 1) Fazer um gráfico da série
+
+# 2) Fazer um gráfico do número médio de mortes por mês no período
+
+
+
+
+
+### Banco de dados: Survival of passengers on the Titanic
+###########################################################
+
+Titanic
+
+class(Titanic)
+
+# Fazer um gráfico de barras do número de sobreviventes/mortos dividindo
+# por classe, gênero e idade.
+
+
+
+
+
+### Banco de dados: New York Air Quality Measurements
+###########################################################
+
+airquality
+
+# 1) Fazer um gráfico pela concentração de ozônio diária para cada mês considerado.
+
+# 2) Investigar indícios de associação entre o ozônio e a temperatura e ozônio
+#    e a velocidade do vento.
+
+
+
+
+
